7cfff62167a5dfad3b27c941c3ba1a91d3b7be45
Sharvil21 <sshastri2102@gmail.com>
2024-01-18 17:27:21 -0500

    Adding another Pandas Solution

--- Solutions/Easy/ID_9662_List_all_companies_working_in_the_financial_sector_with_headquarters_in_Europe_or_Asia_Pandas_Solution.py
+++ Solutions/Easy/ID_9662_List_all_companies_working_in_the_financial_sector_with_headquarters_in_Europe_or_Asia_Pandas_Solution.py
@@ -5,3 +5,5 @@ import pandas as pd
 
 # Start writing code
 forbes_global_2010_2014.loc[(forbes_global_2010_2014['sector']== 'Financials') & ((forbes_global_2010_2014['continent']== 'Asia') | (forbes_global_2010_2014['continent']== 'Europe')),'company']
+
+#
