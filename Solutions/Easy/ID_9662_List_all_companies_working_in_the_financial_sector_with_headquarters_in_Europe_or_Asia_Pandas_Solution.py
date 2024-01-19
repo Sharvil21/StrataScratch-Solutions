6839ab5399a68ae541d22315a6ec32472de08775
Sharvil21 <sshastri2102@gmail.com>
2024-01-19 17:28:10 -0500

    Added another Pandas Solution

--- Solutions/Easy/ID_9662_List_all_companies_working_in_the_financial_sector_with_headquarters_in_Europe_or_Asia_Pandas_Solution.py
+++ Solutions/Easy/ID_9662_List_all_companies_working_in_the_financial_sector_with_headquarters_in_Europe_or_Asia_Pandas_Solution.py
@@ -8,3 +8,7 @@ forbes_global_2010_2014.loc[(forbes_global_2010_2014['sector']== 'Financials') &
 
 #Another Pandas Solution using .query() method
 #WE'll also have to use the .isin() method for filtering for Europe and Asia
+import pandas as pd
+
+df = forbes_global_2010_2014
+result = df[(df['sector'] == 'Financials') & (df['continent'].isin(['Europe', 'Asia']))]['company']
