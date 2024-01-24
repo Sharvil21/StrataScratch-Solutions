9b26043a4e275d7921c96722793e4363953b5b0c
Sharvil21 <sshastri2102@gmail.com>
2024-01-24 17:32:43 -0500

    Added the PostgreSQL Solution

--- Solutions/Easy/ID_9662_List_all_companies_working_in_the_financial_sector_with_headquarters_in_Europe_or_Asia_PostgreSQL_Solution.sql
+++ Solutions/Easy/ID_9662_List_all_companies_working_in_the_financial_sector_with_headquarters_in_Europe_or_Asia_PostgreSQL_Solution.sql
@@ -1,2 +1,7 @@
 --PostgreSQL solution
 --We have to use the WHERE clause to filter the required rows
+SELECT
+company
+FROM
+forbes_global_2010_2014
+WHERE sector = 'Financials' AND (continent = 'Asia' OR continent = 'Europe');
