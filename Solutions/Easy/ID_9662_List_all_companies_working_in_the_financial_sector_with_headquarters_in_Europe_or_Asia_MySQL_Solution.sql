56895e16476f3883da68fa9ce92e2cb0a932eefa
Sharvil21 <sshastri2102@gmail.com>
2024-01-28 18:17:48 -0500

    Added the MySQL Solution

--- Solutions/Easy/ID_9662_List_all_companies_working_in_the_financial_sector_with_headquarters_in_Europe_or_Asia_MySQL_Solution.sql
+++ Solutions/Easy/ID_9662_List_all_companies_working_in_the_financial_sector_with_headquarters_in_Europe_or_Asia_MySQL_Solution.sql
@@ -1,2 +1,7 @@
 --MySQL Solution
 --Use WHERE clause to filter out the Financials sector, and either of the continents being Asia or Europe
+SELECT
+company
+FROM
+forbes_global_2010_2014
+WHERE sector = 'Financials' AND (continent = 'Asia' OR continent = 'Europe');
