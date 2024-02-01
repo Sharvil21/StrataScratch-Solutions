e8d6f3c033d11b5b4d632edc131f9847522a9b23
Sharvil21 <sshastri2102@gmail.com>
2024-02-01 18:23:22 -0500

    Adding the Pandas Solution

--- Solutions/Easy/ID_9663_Most_Profitable_Financial_Company_Pandas_Solution.py
+++ Solutions/Easy/ID_9663_Most_Profitable_Financial_Company_Pandas_Solution.py
@@ -1,2 +1,2 @@
 #Pandas Solution
-#Although there is a column called rank already in the table, I created another column called rnk with .rank() and used the 'dense' method
+#Although there is a column called rank already in the table, I created another column called rnk with .rank() and used the 'dense' method for the profit column
