b43c017270093d577174678f776b1e1c1c7f070a
Sharvil21 <sshastri2102@gmail.com>
2024-02-01 18:24:02 -0500

    Adding the Pandas Solution

--- Solutions/Easy/ID_9663_Most_Profitable_Financial_Company_Pandas_Solution.py
+++ Solutions/Easy/ID_9663_Most_Profitable_Financial_Company_Pandas_Solution.py
@@ -1,2 +1,3 @@
 #Pandas Solution
 #Although there is a column called rank already in the table, I created another column called rnk with .rank() and used the 'dense' method for the profit column
+#All that's left now to do is to use the .loc[] to filter out those rows where the rnk value is 1, and get teh company and continent columns
