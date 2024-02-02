b447fa3e1364c45b89e7627cb1e20e60796a0c2f
Sharvil21 <sshastri2102@gmail.com>
2024-02-02 18:24:12 -0500

    Adding the Pandas Solution

--- Solutions/Easy/ID_9663_Most_Profitable_Financial_Company_Pandas_Solution.py
+++ Solutions/Easy/ID_9663_Most_Profitable_Financial_Company_Pandas_Solution.py
@@ -1,3 +1,9 @@
 #Pandas Solution
 #Although there is a column called rank already in the table, I created another column called rnk with .rank() and used the 'dense' method for the profit column
 #All that's left now to do is to use the .loc[] to filter out those rows where the rnk value is 1, and get teh company and continent columns
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+forbes_global_2010_2014['rnk'] = forbes_global_2010_2014['profits'].rank(method='dense',ascending=False)
+forbes_global_2010_2014.loc[forbes_global_2010_2014['rnk'] == 1,['company','continent']]
