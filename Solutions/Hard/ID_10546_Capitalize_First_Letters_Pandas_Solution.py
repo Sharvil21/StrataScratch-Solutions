08b4c2eacf39a4dca4f5ffd02666edb0f2b1a27d
Sharvil21 <sshastri2102@gmail.com>
2024-02-07 16:54:14 -0500

    ADding the Pandas Solution

--- Solutions/Hard/ID_10546_Capitalize_First_Letters_Pandas_Solution.py
+++ Solutions/Hard/ID_10546_Capitalize_First_Letters_Pandas_Solution.py
@@ -1,2 +1,3 @@
 #Pandas Solution
 #Although this is categorised as hard, the solution is easy
+#We just create a new column, and use the .title() method to make the first letter uppercase of every word
