d25f2e756e4c7232c9d6aebfb9fce2445f385c1c
Sharvil21 <sshastri2102@gmail.com>
2024-08-06 14:44:57 -0400

    Added the Solution

---
+++ Solutions/Easy/ID_2039_Products_Report_Summary.sql
@@ -0,0 +1,7 @@
+SELECT
+product_category, COUNT(DISTINCT transaction_id) AS transactions, SUM(sales) AS sales
+FROM wfm_transactions
+JOIN wfm_products USING(product_id)
+WHERE EXTRACT(YEAR FROM transaction_date) = 2017
+GROUP BY 1
+ORDER BY 3 DESC;
