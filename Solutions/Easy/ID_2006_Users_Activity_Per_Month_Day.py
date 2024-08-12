6999df3ee883df7a9da8ae5dcc38ded450b44647
Sharvil21 <sshastri2102@gmail.com>
2024-08-11 20:32:09 -0400

    Added the second solution

--- Solutions/Easy/ID_2006_Users_Activity_Per_Month_Day.py
+++ Solutions/Easy/ID_2006_Users_Activity_Per_Month_Day.py
@@ -6,3 +6,8 @@ import pandas as pd
 facebook_posts.groupby(facebook_posts['post_date'].dt.day).agg(user_activity=('post_id','count')).reset_index()
 
 #Second Solution
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+facebook_posts.groupby(facebook_posts.post_date.dt.day)['post_id'].count().to_frame().reset_index()
