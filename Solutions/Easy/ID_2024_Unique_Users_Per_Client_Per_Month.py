14d71c53a0e9e81865197a2ce9124bf0c0facdd3
Sharvil21 <sshastri2102@gmail.com>
2024-09-05 18:44:02 -0400

    Added the first solution

--- Solutions/Easy/ID_2024_Unique_Users_Per_Client_Per_Month.py
+++ Solutions/Easy/ID_2024_Unique_Users_Per_Client_Per_Month.py
@@ -1,1 +1,7 @@
 #First Solution:
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+fact_events['month'] = fact_events['time_id'].dt.month
+fact_events.groupby(['client_id','month'])['user_id'].nunique().reset_index()
