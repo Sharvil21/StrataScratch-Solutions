da34d30ae1c60e069bdb49e1601f928aea8618f8
Sharvil21 <sshastri2102@gmail.com>
2024-09-05 18:31:43 -0400

    Added the solution

--- Solutions/Easy/ID_2018_Inactive_Free_Users.py
+++ Solutions/Easy/ID_2018_Inactive_Free_Users.py
@@ -1,1 +1,9 @@
 #Solution using .query() method
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+free_users = rc_users.query('status=="free"')
+users_in_april = rc_calls.query("date >= '2020-04-01' and date <= '2020-04-30'")['user_id'].unique()
+
+free_users.query("user_id not in @users_in_april")['user_id'].unique()
