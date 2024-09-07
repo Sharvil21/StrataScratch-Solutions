14188bbd0a6c921eb06d10995c054379561b325f
Sharvil21 <sshastri2102@gmail.com>
2024-09-07 19:04:04 -0400

    Adding the MySQL Solution

--- Solutions/Easy/ID_2039_Products_Report_Summary.sql
+++ Solutions/Easy/ID_2039_Products_Report_Summary.sql
@@ -8,4 +8,11 @@ GROUP BY 1
 ORDER BY 3 DESC;
 
 --Solution using MySQL:
-
+SELECT
+product_category, COUNT(DISTINCT transaction_id) AS transactions, SUM(sales) AS sales
+FROM wfm_transactions
+JOIN wfm_products
+USING(product_id)
+WHERE EXTRACT(YEAR FROM transaction_date) = 2017
+GROUP BY 1
+ORDER BY 3 DESC;
