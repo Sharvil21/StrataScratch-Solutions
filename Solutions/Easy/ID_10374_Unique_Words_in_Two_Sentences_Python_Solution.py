7e4f596d0b5d846ba4f427cb988e28598f745038
Sharvil21 <sshastri2102@gmail.com>
2024-12-26 13:21:46 -0500

    Adding the Python Solution

--- Solutions/Easy/ID_10374_Unique_Words_in_Two_Sentences_Python_Solution.py
+++ Solutions/Easy/ID_10374_Unique_Words_in_Two_Sentences_Python_Solution.py
@@ -1,2 +1,17 @@
 #Python Solution
 #Have to use the Counter from collections library
+from collections import Counter
+def non_repeated_words(input):
+    """
+    :type input: List[str]
+    :rtype: List[str]
+    """
+
+    sentence1 = input[0]
+    sentence2 = input[1]
+    count = Counter(sentence1.split() + sentence2.split())
+    output = []
+    for i in count:
+        if count[i] == 1:
+            output.append(i)
+    return output
