020f398bc1f283e7d8e6dfa39328b7e0198683ae
Sharvil21 <sshastri2102@gmail.com>
2024-12-27 13:27:23 -0500

    Added the Python Solution

--- Solutions/Easy/ID_10379_Replace_Nulls_with_Preceding_Non-nulls_Python_Solution.py
+++ Solutions/Easy/ID_10379_Replace_Nulls_with_Preceding_Non-nulls_Python_Solution.py
@@ -1,1 +1,10 @@
 #Python Solution
+def replace_null_values(lst):
+    """ 
+    :type lst: List[Union[int, None]]
+    :rtype: List[Union[int, None]]
+    """
+    for i in range(1,len(lst)):
+        if lst[i] == None:
+            lst[i] = lst[i-1]
+    return lst
