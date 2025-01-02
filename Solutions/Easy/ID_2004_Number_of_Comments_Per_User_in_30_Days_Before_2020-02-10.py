95b572750e1714f7a0bfc17d138ae5ce48b815ee
Sharvil21 <sshastri2102@gmail.com>
2025-01-01 19:49:45 -0500

    Added the first solution

--- Solutions/Easy/ID_2004_Number_of_Comments_Per_User_in_30_Days_Before_2020-02-10.py
+++ Solutions/Easy/ID_2004_Number_of_Comments_Per_User_in_30_Days_Before_2020-02-10.py
@@ -1,1 +1,8 @@
 #First Solution
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+filtered_df = fb_comments_count.query("created_at > '2020-01-10' and created_at <= '2020-02-10'")
+
+filtered_df.groupby('user_id').agg(number_of_comments=('number_of_comments','sum')).reset_index()
