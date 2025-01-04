dd9ac58df0104e7baaff351c1342f49de6cc8f92
Sharvil21 <sshastri2102@gmail.com>
2025-01-04 18:20:37 -0500

    Added the Pandas Solution

--- Solutions/Easy/ID_2051_Monthly_Active_Users_Pandas_Solution.py
+++ Solutions/Easy/ID_2051_Monthly_Active_Users_Pandas_Solution.py
@@ -1,1 +1,9 @@
 #Pandas Solution
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+sf_events['month'] = sf_events['record_date'].dt.month
+sf_events['year'] = sf_events['record_date'].dt.year
+
+sf_events.query("month==1 and year == 2021").groupby(['month','year','account_id'])['user_id'].nunique().reset_index()[['account_id','user_id']]
