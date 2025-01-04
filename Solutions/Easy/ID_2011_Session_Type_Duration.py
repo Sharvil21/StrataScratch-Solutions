1467903d0600787c29875595097bafa14c78efe6
Sharvil21 <sshastri2102@gmail.com>
2025-01-04 16:09:50 -0500

    Added the solution

--- Solutions/Easy/ID_2011_Session_Type_Duration.py
+++ Solutions/Easy/ID_2011_Session_Type_Duration.py
@@ -1,1 +1,8 @@
 #Solution
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+twitch_sessions['duration'] = (twitch_sessions['session_end']- twitch_sessions['session_start']).dt.total_seconds()
+
+twitch_sessions.groupby('session_type')['duration'].mean().reset_index()
