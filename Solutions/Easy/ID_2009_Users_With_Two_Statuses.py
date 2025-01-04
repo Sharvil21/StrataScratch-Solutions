88b0fc538132ed68f936c1959f44f2deeb35c85b
Sharvil21 <sshastri2102@gmail.com>
2025-01-04 15:10:28 -0500

    Added the second solution

--- Solutions/Easy/ID_2009_Users_With_Two_Statuses.py
+++ Solutions/Easy/ID_2009_Users_With_Two_Statuses.py
@@ -8,3 +8,10 @@ df2 = twitch_sessions.groupby('user_id')['session_type'].nunique().reset_index()
 df2[df2['session_type'] > 1]['user_id']
 
 #Second Solution
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+result = twitch_sessions.groupby('user_id')['session_type'].nunique().to_frame('n_types').reset_index()
+result = result[result['n_types'] == 2]['user_id']
+
