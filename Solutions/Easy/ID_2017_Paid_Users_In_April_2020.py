ae89b6d374b541db909e2cd3ba88767d0edc54df
Sharvil21 <sshastri2102@gmail.com>
2025-01-05 16:54:42 -0500

    Added the solution

--- Solutions/Easy/ID_2017_Paid_Users_In_April_2020.py
+++ Solutions/Easy/ID_2017_Paid_Users_In_April_2020.py
@@ -1,1 +1,8 @@
 #Solution using .query() method.
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+df = rc_calls.merge(rc_users,on='user_id',how='inner').query("status=='paid' and date >= '2020-04-01' and date <= '2020-04-30'")
+
+df['user_id'].nunique()
