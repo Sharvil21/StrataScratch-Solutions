eda0d06dc3087304aea90adfae1fe2a0ccb4dee1
Sharvil21 <sshastri2102@gmail.com>
2025-01-07 18:24:31 -0500

    Added the second Pandas Solution

--- Solutions/Easy/ID_2051_Monthly_Active_Users_Pandas_Solution.py
+++ Solutions/Easy/ID_2051_Monthly_Active_Users_Pandas_Solution.py
@@ -9,3 +9,11 @@ sf_events['year'] = sf_events['record_date'].dt.year
 sf_events.query("month==1 and year == 2021").groupby(['month','year','account_id'])['user_id'].nunique().reset_index()[['account_id','user_id']]
 
 #2nd Solution
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+
+df = sf_events.loc[(sf_events['record_date'].dt.month==1) & (sf_events['record_date'].dt.year ==2021)]
+
+df.groupby(['account_id'])['user_id'].nunique().to_frame().reset_index()
