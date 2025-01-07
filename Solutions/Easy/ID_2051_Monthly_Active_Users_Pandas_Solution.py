8a1b8c4d049a371a1f0cd948983923df397537bd
Sharvil21 <sshastri2102@gmail.com>
2025-01-07 18:21:23 -0500

    Adding the second Pandas Solution

--- Solutions/Easy/ID_2051_Monthly_Active_Users_Pandas_Solution.py
+++ Solutions/Easy/ID_2051_Monthly_Active_Users_Pandas_Solution.py
@@ -7,3 +7,5 @@ sf_events['month'] = sf_events['record_date'].dt.month
 sf_events['year'] = sf_events['record_date'].dt.year
 
 sf_events.query("month==1 and year == 2021").groupby(['month','year','account_id'])['user_id'].nunique().reset_index()[['account_id','user_id']]
+
+#
