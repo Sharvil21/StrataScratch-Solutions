efcae0cf5dd1ab56ae1ef0609885ddb2c3b6e791
Sharvil21 <sshastri2102@gmail.com>
2025-01-11 16:39:39 -0500

    Added the first MySQL Solution

--- Solutions/Easy/ID_2043_Employees_Without_Annual_Review_MySQL_Solution.sql
+++ Solutions/Easy/ID_2043_Employees_Without_Annual_Review_MySQL_Solution.sql
@@ -1,1 +1,14 @@
 --First MySQL Solution
+WITH cte AS (SELECT
+a.first_name, a.id, COUNT(b.review_date) AS total_reviews
+FROM uber_employees a
+LEFT JOIN uber_annual_review b
+ON a.id = b.emp_id
+GROUP BY 1,2
+HAVING COUNT(b.review_date) = 0)
+
+SELECT 
+first_name, last_name, hire_date, termination_date
+FROM uber_employees
+WHERE id IN (SELECT id FROM cte)
+ORDER BY hire_date DESC;
