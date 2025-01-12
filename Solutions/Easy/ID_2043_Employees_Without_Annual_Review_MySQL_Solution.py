cd27a8e1a41f1edf30c6ec31f32b06973930da08
Sharvil21 <sshastri2102@gmail.com>
2025-01-12 17:17:23 -0500

    Added the Pandas Solution

--- Solutions/Easy/ID_2043_Employees_Without_Annual_Review_MySQL_Solution.py
+++ Solutions/Easy/ID_2043_Employees_Without_Annual_Review_MySQL_Solution.py
@@ -1,2 +1,12 @@
 #Pandas Solution
+# Import your libraries
+import pandas as pd
+
+
+
+df = uber_employees.merge(uber_annual_review,left_on='id',right_on='emp_id',how='left')
+filtered_df = df.groupby('id_x')['review_date'].count().reset_index()
+required_ids = filtered_df.query("review_date == 0")['id_x']
+y = list(required_ids)
+uber_employees.query(f"id.isin({y})")[['first_name','last_name','hire_date','termination_date']].sort_values(by='hire_date',ascending=False)
 
