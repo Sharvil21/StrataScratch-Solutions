a4f38479cbdc203dc1bc366982e186df1c49c637
Sharvil21 <sshastri2102@gmail.com>
2025-01-17 17:27:37 -0500

    Added the second Pandas Solution

--- Solutions/Easy/2049_Total_Order_Per_Status_Per_Service.py
+++ Solutions/Easy/2049_Total_Order_Per_Status_Per_Service.py
@@ -6,3 +6,8 @@ import pandas as pd
 uber_orders.groupby(['service_name','status_of_order'])['number_of_orders'].sum().to_frame('orders_sum').reset_index()
 
 #Solution with .agg() method
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+uber_orders.groupby(['service_name','status_of_order']).agg({'number_of_orders':['sum']}).reset_index()
