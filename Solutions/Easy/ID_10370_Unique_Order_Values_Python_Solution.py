1d60237b58aa1f90d541bf8cfac7bc2fb3500da3
Sharvil21 <sshastri2102@gmail.com>
2025-03-14 17:27:07 -0400

    Added another Python Solution

--- Solutions/Easy/ID_10370_Unique_Order_Values_Python_Solution.py
+++ Solutions/Easy/ID_10370_Unique_Order_Values_Python_Solution.py
@@ -12,3 +12,20 @@ def non_duplicate(input):
     return lst
 
 #Another Python Solution
+def non_duplicate(input):
+    """ 
+    :type input: List[int]
+    :rtype: List[int]
+    """
+    
+    seen = set()
+    result = []
+    
+    for n in input:
+        if n not in seen:
+            seen.add(n)
+            result.append(n)
+            
+    return result
+    
+
