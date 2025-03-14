ee28878d554526e7ae03252425cfb511c8fdefce
Sharvil21 <sshastri2102@gmail.com>
2025-03-14 17:26:12 -0400

    Added the Python Solution

--- Solutions/Easy/ID_10370_Unique_Order_Values_Python_Solution.py
+++ Solutions/Easy/ID_10370_Unique_Order_Values_Python_Solution.py
@@ -1,1 +1,12 @@
 #Python Solution
+def non_duplicate(input):
+    """ 
+    :type input: List[int]
+    :rtype: List[int]
+    """
+    lst = []
+    for i in input:
+        if i not in lst:
+            lst.append(i)
+            
+    return lst
