193c52388a2a9a2981ccbef7e72c368926796244
Sharvil21 <sshastri2102@gmail.com>
2025-03-25 16:35:07 -0400

    Adding the Pandas Solution

--- Solutions/Easy/ID_10127_Calculate_Samanthas_and_Lisas_total_sales_revenue_Pandas_Solution.py
+++ Solutions/Easy/ID_10127_Calculate_Samanthas_and_Lisas_total_sales_revenue_Pandas_Solution.py
@@ -1,3 +1,8 @@
 #Pandas Solution
 #Can just use .loc[] to get the rows directly where salesperson is Samantha Or Lisa
 #Then we can just take the sum() of sales_revenue column
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+sales_performance.loc[(sales_performance['salesperson'] == 'Samantha') | (sales_performance['salesperson'] == 'Lisa'),'sales_revenue'].sum()
