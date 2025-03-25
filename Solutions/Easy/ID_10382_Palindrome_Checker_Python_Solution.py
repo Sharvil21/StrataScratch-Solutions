012b0ffee0145784fa31e46b008c7818cf7c4632
Sharvil21 <sshastri2102@gmail.com>
2025-03-25 16:19:06 -0400

    Added the Python Solution

--- Solutions/Easy/ID_10382_Palindrome_Checker_Python_Solution.py
+++ Solutions/Easy/ID_10382_Palindrome_Checker_Python_Solution.py
@@ -1,2 +1,8 @@
 #Python Solution
 #Just gotta use string indexing and slicing directly to get the output
+def is_palindrome(s):
+    """ 
+    :type s: str
+    :rtype: bool
+    """
+    return s == s[::-1]
