3bae1afa550bd0c98fb40cf530e1bbf3ae4267f9
Sharvil21 <sshastri2102@gmail.com>
2025-03-26 18:34:45 -0400

    Adding the Pandas Solution

--- Solutions/Medium/ID_2003_Recent_Refinance_Submissions_Pandas_Solution.py
+++ Solutions/Medium/ID_2003_Recent_Refinance_Submissions_Pandas_Solution.py
@@ -3,3 +3,4 @@
 #Then, we'll have to first filter out those rows where the type is 'Refinance' first
 #AFter that, we create a new column for the most recent date.
 #This can be done by using .transform('max') method after .groupby()
+#Lastly filter this dataframe where the created_at date is the same as the most recent date, and return the user_id as well as balance
