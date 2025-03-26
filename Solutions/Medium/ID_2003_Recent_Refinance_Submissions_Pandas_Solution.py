bff2f40857f60351d5e593ca937ed816459eb597
Sharvil21 <sshastri2102@gmail.com>
2025-03-26 18:33:41 -0400

    Adding the Pandas Solution

--- Solutions/Medium/ID_2003_Recent_Refinance_Submissions_Pandas_Solution.py
+++ Solutions/Medium/ID_2003_Recent_Refinance_Submissions_Pandas_Solution.py
@@ -1,4 +1,5 @@
 #Pandas Solution
 #First, we merge the dataframe using an innerjoin
 #Then, we'll have to first filter out those rows where the type is 'Refinance' first
-#AFter that, we create a new column for the most recent date. 
+#AFter that, we create a new column for the most recent date.
+#This can be done by using .transform('max') method after .groupby()
