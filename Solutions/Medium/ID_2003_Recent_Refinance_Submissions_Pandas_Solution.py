9b11e597f1a4132fbdf7ad045b8b6ffc35bc27cb
Sharvil21 <sshastri2102@gmail.com>
2025-03-26 18:35:10 -0400

    Added the Pandas Solution

--- Solutions/Medium/ID_2003_Recent_Refinance_Submissions_Pandas_Solution.py
+++ Solutions/Medium/ID_2003_Recent_Refinance_Submissions_Pandas_Solution.py
@@ -5,3 +5,11 @@
 #This can be done by using .transform('max') method after .groupby()
 #Lastly filter this dataframe where the created_at date is the same as the most recent date, and return the user_id as well as balance
 #We'd be creating two dataframes for this solution
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+df = loans.merge(submissions,left_on='id',right_on='loan_id',how='inner')
+df2 = df.loc[df['type']=='Refinance']
+df2['most_recent_date'] = df2.groupby('user_id')['created_at'].transform('max')
+df2.loc[df2['created_at']==df2['most_recent_date'],['user_id','balance']]
