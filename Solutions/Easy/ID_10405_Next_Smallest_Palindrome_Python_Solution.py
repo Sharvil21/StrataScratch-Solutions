5d11953944dcd3f4b067d16be8aab6bf82bf96a6
Sharvil21 <sshastri2102@gmail.com>
2025-03-27 17:39:03 -0400

    Added the Python solution

--- Solutions/Easy/ID_10405_Next_Smallest_Palindrome_Python_Solution.py
+++ Solutions/Easy/ID_10405_Next_Smallest_Palindrome_Python_Solution.py
@@ -1,3 +1,11 @@
 #Python Solution
 #Since a Palindrome is involved, we'll have to use indexing and slicing of a string
 #We use a for loop in this case, then convert the number to a string. If the reverse of that string is true, return that number
+def next_palindrome(n):
+    """
+    :type n: int
+    :rtype: int
+    """
+    for i in range(n+1,10000000):
+        if str(i) == str(i)[::-1]:
+            return i
