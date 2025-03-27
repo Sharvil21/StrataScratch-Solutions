a3c970bc9a9858ad4a5a0b92e9e95ba36fa45814
Sharvil21 <sshastri2102@gmail.com>
2025-03-27 17:38:52 -0400

    Adding the Python solution

--- Solutions/Easy/ID_10405_Next_Smallest_Palindrome_Python_Solution.py
+++ Solutions/Easy/ID_10405_Next_Smallest_Palindrome_Python_Solution.py
@@ -1,2 +1,3 @@
 #Python Solution
 #Since a Palindrome is involved, we'll have to use indexing and slicing of a string
+#We use a for loop in this case, then convert the number to a string. If the reverse of that string is true, return that number
