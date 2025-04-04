2376aa80e6845c593ef8bda18dec1ea3616d565e
Sharvil21 <sshastri2102@gmail.com>
2025-04-04 13:55:57 -0400

    Added the Pandas Solution

--- Solutions/Easy/ID_2119_Most_Lucrative_Products_Pandas_Solution.py
+++ Solutions/Easy/ID_2119_Most_Lucrative_Products_Pandas_Solution.py
@@ -1,2 +1,9 @@
 #Pandas Solution
+# Import your libraries
+import pandas as pd
 
+# Start writing code
+df = online_orders.query("date_sold.dt.year == 2022 and date_sold.dt.month >= 1 and date_sold.dt.month <= 6 ")
+df['revenue'] = df['units_sold']*df['cost_in_dollars']
+
+df.groupby('product_id',as_index=False).agg(total_revenue=('revenue','sum')).sort_values(by='total_revenue',ascending=False).iloc[:5]
