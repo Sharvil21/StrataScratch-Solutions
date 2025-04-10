411a10ebdadb161b113bfc9eb78d6a8708bd2a34
Sharvil21 <sshastri2102@gmail.com>
2025-04-10 16:49:59 -0400

    Added the Python Solution

--- Solutions/Easy/ID_10401_Digit_Summing_Challenge_Python_Solution.py
+++ Solutions/Easy/ID_10401_Digit_Summing_Challenge_Python_Solution.py
@@ -1,1 +1,14 @@
 #Python Solution
+def sum_digits(numbers):
+    """ 
+    :type numbers: List[int]
+    :rtype: List[int]
+    """
+    ip = [i if i>0 else i*-1 for i in numbers]
+    op = []
+    for i in ip:
+        x = 0
+        for j in str(i):
+            x += int(j)
+        op.append(x)
+    return op
