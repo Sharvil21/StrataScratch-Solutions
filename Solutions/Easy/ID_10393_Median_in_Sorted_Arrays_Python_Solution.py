47c0e30397df6ed7b746bc7a8d1bc23993d36aa5
Sharvil21 <sshastri2102@gmail.com>
2025-04-11 16:15:52 -0400

    Added the Python Solution

--- Solutions/Easy/ID_10393_Median_in_Sorted_Arrays_Python_Solution.py
+++ Solutions/Easy/ID_10393_Median_in_Sorted_Arrays_Python_Solution.py
@@ -1,1 +1,12 @@
 #Python Solution
+import statistics
+def find_median_sorted_arrays(input):
+    """ 
+    :type input: Dict[str, List[int]]
+    :rtype: float
+    """
+
+    nums1 = input["nums1"]
+    nums2 = input["nums2"]
+    
+    return statistics.median(nums1 + nums2)
