9c6332fc18d9b8c384aa7e24b1b213d857c7f773
Sharvil21 <sshastri2102@gmail.com>
2025-04-12 12:07:05 -0400

    Adding the Python Solution

--- Solutions/Medium/ID_10415_Reverse_Letters_Preserve_Words_Python_Solution.py
+++ Solutions/Medium/ID_10415_Reverse_Letters_Preserve_Words_Python_Solution.py
@@ -1,4 +1,5 @@
 #Python Solution
 #Approach is to first split the string into a list, using .split() method
 #Then iterate through a for loop.
+#Then add the reverse of the iteration every time into an empty list (use .append() method)
 
