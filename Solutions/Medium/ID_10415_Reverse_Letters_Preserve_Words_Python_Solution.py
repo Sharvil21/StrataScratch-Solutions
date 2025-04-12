54488c349aaacedcce778c28fc3a3d33750ed909
Sharvil21 <sshastri2102@gmail.com>
2025-04-12 12:07:45 -0400

    Added the Python Solution

--- Solutions/Medium/ID_10415_Reverse_Letters_Preserve_Words_Python_Solution.py
+++ Solutions/Medium/ID_10415_Reverse_Letters_Preserve_Words_Python_Solution.py
@@ -3,3 +3,13 @@
 #Then iterate through a for loop.
 #Then add the reverse of the iteration every time into an empty list (use .append() method)
 #Lastly, use the .join() method to construct the required string which is the output
+def reverse_letters(input_string):
+    """ 
+    :type input_string: str
+    :rtype: str
+    """
+    op = []
+    for i in input_string.split():
+        op.append(i[::-1])
+    
+    return " ".join(op)
