3ec59fe7bb8a7f700ff2ad4d4c9421e7bb85963c
Sharvil21 <sshastri2102@gmail.com>
2025-04-13 10:27:36 -0400

    Adding another Python Solution

--- Solutions/Medium/ID_10412_Unique_Intersection_of_Integer_Arrays_Python_Solution.py
+++ Solutions/Medium/ID_10412_Unique_Intersection_of_Integer_Arrays_Python_Solution.py
@@ -15,3 +15,13 @@ def find_intersection(input):
     return list(set(op))
 
 #Another Python solution
+def find_intersection(input):
+    """ 
+    :type input: Dict[str, List[int]]
+    :rtype: List[int]
+    """
+
+    arr1 = input["arr1"]
+    arr2 = input["arr2"]
+    
+    return list(set(arr1) & set(arr2))
