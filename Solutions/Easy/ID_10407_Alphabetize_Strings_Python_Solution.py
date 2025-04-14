5fd5886676c7b85729a7a8df65d7f2d39e5bfdb8
Sharvil21 <sshastri2102@gmail.com>
2025-04-14 13:50:54 -0400

    Added the Python Solution

--- Solutions/Easy/ID_10407_Alphabetize_Strings_Python_Solution.py
+++ Solutions/Easy/ID_10407_Alphabetize_Strings_Python_Solution.py
@@ -7,3 +7,4 @@ def sort_string(string):
     return "".join(sorted(string))
 
 #In the above solution, use the sorted() function to sort the string. But this will first put the alphabets in a list then sort it.
+#So to get the desired string back, use the "".join() method and return it
