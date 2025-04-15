ab812d2e3577cdf4ffd1c86ac2047bc49aad07cf
Sharvil21 <sshastri2102@gmail.com>
2025-04-15 13:59:07 -0400

    Added the Python Solution

--- Solutions/Medium/ID_10394_Sorted_Square_Array_Python_Solution.py
+++ Solutions/Medium/ID_10394_Sorted_Square_Array_Python_Solution.py
@@ -1,6 +1,6 @@
 #Python Solution
 #In this, I just used List Comprehension
-#Then lastly used sorted() function to return the desired output
+#Then lastly used sorted() function to return the desired output, which requires us to square the numbers in a list and return a sorted list
 def sorted_squares(arr):
     """ 
     :type arr: List[int]
