c283c9c4ec46ee5e1a0553b961eedeaf79fbcd27
Sharvil21 <sshastri2102@gmail.com>
2025-04-15 13:58:32 -0400

    Adding the Python Solution

--- Solutions/Medium/ID_10394_Sorted_Square_Array_Python_Solution.py
+++ Solutions/Medium/ID_10394_Sorted_Square_Array_Python_Solution.py
@@ -1,5 +1,6 @@
 #Python Solution
 #In this, I just used List Comprehension
+#Then lastly used sorted() function to return the desired output
 def sorted_squares(arr):
     """ 
     :type arr: List[int]
