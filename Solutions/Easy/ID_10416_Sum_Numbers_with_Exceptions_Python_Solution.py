9dbe18811cc5e70d944324f3c7d933a0c8178042
Sharvil21 <sshastri2102@gmail.com>
2025-04-16 12:26:06 -0400

    Adding the Python Solution

--- Solutions/Easy/ID_10416_Sum_Numbers_with_Exceptions_Python_Solution.py
+++ Solutions/Easy/ID_10416_Sum_Numbers_with_Exceptions_Python_Solution.py
@@ -1,3 +1,3 @@
 #Python Solution
 #I just used the sum() function with list comprehension
-#For the list comprehension, I had to use 'and' operator in the if condition as both conditions must be true
+#For the list comprehension, I had to use 'and' operator in the if condition as both conditions must be true to add the number into the list
