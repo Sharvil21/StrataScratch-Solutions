8232c0b0a3e960e7b468c3240b2b2e7c802b1ce5
Sharvil21 <sshastri2102@gmail.com>
2025-04-16 12:26:15 -0400

    Added the Python Solution

--- Solutions/Easy/ID_10416_Sum_Numbers_with_Exceptions_Python_Solution.py
+++ Solutions/Easy/ID_10416_Sum_Numbers_with_Exceptions_Python_Solution.py
@@ -1,3 +1,9 @@
 #Python Solution
 #I just used the sum() function with list comprehension
 #For the list comprehension, I had to use 'and' operator in the if condition as both conditions must be true to add the number into the list
+def calculate_sum(N):
+    """ 
+    :type N: int
+    :rtype: int
+    """
+    return sum([i if (i%3 != 0 and i%7 !=0) else 0 for i in range(0,N+1)])
