d869022ea63be3e116a741c293750571c55d0a91
Sharvil21 <sshastri2102@gmail.com>
2025-04-25 11:58:36 -0400

    Added the Python Solution

--- Solutions/Medium/ID_10381_Zeroes_Shifted_Order_Maintained_Python_Solution.py
+++ Solutions/Medium/ID_10381_Zeroes_Shifted_Order_Maintained_Python_Solution.py
@@ -2,3 +2,13 @@
 #There is no specific requirement to change the order in place, or anything as such
 #So I use list comprehension to just add the non-zero numbers first. They are in order
 #Then iterate through a for loop and add zeroes to the end using the .append() method
+def shift_zeroes(arr):
+    """ 
+    :type arr: List[int]
+    :rtype: List[int]
+    """
+    op = [i for i in arr if i != 0]
+    for i in arr:
+        if i == 0:
+            op.append(i)
+    return op
