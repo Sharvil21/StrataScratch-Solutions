4fc59b36cbe307f67ecad20229a04cec8111b40a
Sharvil21 <sshastri2102@gmail.com>
2025-04-24 21:08:20 -0400

    Adding the Python Solution

--- Solutions/Hard/ID_10384_Prime_Numbers_Below_N_Python_Solution.py
+++ Solutions/Hard/ID_10384_Prime_Numbers_Below_N_Python_Solution.py
@@ -1,1 +1,17 @@
 #Python Solutionm
+def get_primes(n):
+    """ 
+    :type n: int
+    :rtype: List[int]
+    """
+    output = []
+    def isprime(num):
+        for i in range(2,int(num**0.5)+1):
+            if num%i == 0:
+                return False
+        return True
+        
+    for i in range(2,n):
+        if isprime(i) == True:
+            output.append(i)
+    return output
