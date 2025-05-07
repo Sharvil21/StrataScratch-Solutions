712192c353d20f4fa470aacc310ea38ce1f4d45c
Sharvil21 <sshastri2102@gmail.com>
2025-05-07 18:23:29 -0400

    Added the Pandas Solution

--- Solutions/Medium/ID_9927_Libraries_With_Highest_Checkouts_Pandas_Solution.py
+++ Solutions/Medium/ID_9927_Libraries_With_Highest_Checkouts_Pandas_Solution.py
@@ -1,3 +1,12 @@
 #Pandas Solution
 #Filter out the rows first
 #Then groupby.
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+library_usage.head()
+
+df = library_usage.loc[(library_usage['age_range'] == '65 to 74 years') & (library_usage['circulation_active_month'] == 'April') & (library_usage['year_patron_registered'] == 2015)]
+
+df.groupby(['year_patron_registered','home_library_definition'],as_index=False).agg(max_total_checkouts=('total_checkouts','max')).sort_values(by='max_total_checkouts',ascending=False)
