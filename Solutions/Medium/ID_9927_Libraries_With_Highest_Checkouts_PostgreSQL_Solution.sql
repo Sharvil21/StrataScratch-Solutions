d3491b54c36e936c0a235d260425d72ed69f8c5e
Sharvil21 <sshastri2102@gmail.com>
2025-05-07 18:14:33 -0400

    Added the PostgreSQL Solution

--- Solutions/Medium/ID_9927_Libraries_With_Highest_Checkouts_PostgreSQL_Solution.sql
+++ Solutions/Medium/ID_9927_Libraries_With_Highest_Checkouts_PostgreSQL_Solution.sql
@@ -1,1 +1,7 @@
 -- PostgreSQL Solution
+SELECT
+year_patron_registered, home_library_definition, MAX(total_checkouts) AS max_total_checkouts
+FROM library_usage
+WHERE age_range = '65 to 74 years' AND year_patron_registered = 2015 AND circulation_active_month = 'April'
+GROUP BY 1,2
+ORDER BY 3 DESC;
