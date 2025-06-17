b03d3528981fb5191a057788c62e5f03c9a89ef2
Sharvil21 <sshastri2102@gmail.com>
2025-06-17 11:17:57 -0400

    Adding the PostgreSQL Solution

--- Solutions/Easy/ID_9917_Average_Salaries_PostgreSQL_Solution.sql
+++ Solutions/Easy/ID_9917_Average_Salaries_PostgreSQL_Solution.sql
@@ -1,1 +1,3 @@
 --PostgreSQL Solution
+--Have to use a window function here along with an aggregate function to get the column that shows the average salary for each department beside the salary of each employee
+ 
