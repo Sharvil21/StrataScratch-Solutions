dc94d540fb935021032a60aa934d3a3ea68eabf7
Sharvil21 <sshastri2102@gmail.com>
2025-06-17 23:50:38 -0400

    Added the PostgreSQL Solution

--- Solutions/Medium/ID_2095_Three_Purchases_PostgreSQL_Solution.sql
+++ Solutions/Medium/ID_2095_Three_Purchases_PostgreSQL_Solution.sql
@@ -1,2 +1,13 @@
 --PostgreSQL Solution
+WITH cte AS (SELECT
+user_id,
+SUM(CASE WHEN EXTRACT(YEAR FROM order_date) = 2021 THEN 1 ELSE 0 END) AS purchases_in_2021,
+SUM(CASE WHEN EXTRACT(YEAR FROM order_date) = 2020 THEN 1 ELSE 0 END) AS purchases_in_2020
+FROM amazon_orders
+GROUP BY user_id)
+
+SELECT
+user_id
+FROM cte
+WHERE purchases_in_2021 > 2 AND purchases_in_2020 > 2;
 
