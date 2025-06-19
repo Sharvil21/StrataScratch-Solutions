b6d16d12429d5677eba72bcf524e662dc6a78578
Sharvil21 <sshastri2102@gmail.com>
2025-06-19 14:51:15 -0400

    Adding the PostgreSQL Solution

--- Solutions/Easy/ID_10308_Salaries_Difference_PostgreSQL_Solution.sql
+++ Solutions/Easy/ID_10308_Salaries_Difference_PostgreSQL_Solution.sql
@@ -1,1 +1,7 @@
 --PostgreSQL Solution
+SELECT
+ABS(MAX(CASE WHEN department = 'marketing' THEN salary ELSE 0 END) -
+MAX(CASE WHEN department = 'engineering' THEN salary ELSE 0 END)) AS salary_difference
+FROM db_employee e
+JOIN db_dept d ON e.department_id = d.id;
+
