05241a9b8f7ce6a956d2135494e635d769369ead
Sharvil21 <sshastri2102@gmail.com>
2025-06-19 13:48:20 -0400

    Added the PostgreSQL Solution

--- Solutions/Hard/ID_10712_Best_Selling_Item_PostgreSQL_Solution.sql
+++ Solutions/Hard/ID_10712_Best_Selling_Item_PostgreSQL_Solution.sql
@@ -1,2 +1,9 @@
 --PostgreSQL Solution
 -- Logic is to use Dense Rank and partition by the month and then later group by the month and description. Assign RANK in descending order then in the main query, select only those rows where the rank is 1
+WITH cte1 AS (SELECT
+EXTRACT(MONTH FROM invoicedate) AS month, description, SUM(unitprice * quantity) AS total_paid, DENSE_RANK() OVER(PARTITION BY EXTRACT(MONTH FROM invoicedate) ORDER BY SUM(unitprice*quantity) DESC) as rnk
+FROM online_retail
+GROUP BY 1,2)
+SELECT
+month, description, total_paid
+FROM cte1 WHERE rnk = 1;
