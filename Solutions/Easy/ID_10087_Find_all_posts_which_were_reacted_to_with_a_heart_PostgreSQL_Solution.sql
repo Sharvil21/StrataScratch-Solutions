306fdec2ef12d6a0037015e6ac6467fedce58cb4
Sharvil21 <sshastri2102@gmail.com>
2025-06-25 14:19:53 -0400

    Adding the PostgreSQL Solution

--- Solutions/Easy/ID_10087_Find_all_posts_which_were_reacted_to_with_a_heart_PostgreSQL_Solution.sql
+++ Solutions/Easy/ID_10087_Find_all_posts_which_were_reacted_to_with_a_heart_PostgreSQL_Solution.sql
@@ -1,4 +1,4 @@
 --PostgreSQL Solution
 --Join the two tables first on post_id
 --Use the WHERE clause to filter out the condition where reaction = 'heart'
---
+--Then SELECT DISTINCT a.* for the result
