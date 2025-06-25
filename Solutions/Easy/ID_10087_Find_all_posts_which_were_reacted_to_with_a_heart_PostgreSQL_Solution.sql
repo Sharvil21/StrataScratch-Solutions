385884edd642eda294f9080805ab5205d4d9d894
Sharvil21 <sshastri2102@gmail.com>
2025-06-25 14:20:03 -0400

    Added the PostgreSQL Solution

--- Solutions/Easy/ID_10087_Find_all_posts_which_were_reacted_to_with_a_heart_PostgreSQL_Solution.sql
+++ Solutions/Easy/ID_10087_Find_all_posts_which_were_reacted_to_with_a_heart_PostgreSQL_Solution.sql
@@ -2,3 +2,9 @@
 --Join the two tables first on post_id
 --Use the WHERE clause to filter out the condition where reaction = 'heart'
 --Then SELECT DISTINCT a.* for the result
+SELECT
+DISTINCT a.*
+FROM facebook_posts a
+JOIN facebook_reactions b
+ON a.post_id = b.post_id
+WHERE b.reaction ~* 'heart';
