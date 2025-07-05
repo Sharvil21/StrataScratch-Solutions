525295d4841d4902dfa578fceb948684ee0ed0d1
Sharvil21 <sshastri2102@gmail.com>
2025-07-05 14:01:17 -0400

    Added the Pandas Solution

--- Solutions/Easy/ID_10277_Find_all_inspections_which_are_part_of_an_inactive_program_pandas_solution.py
+++ Solutions/Easy/ID_10277_Find_all_inspections_which_are_part_of_an_inactive_program_pandas_solution.py
@@ -1,3 +1,4 @@
 #Pandas Solution
 import pandas as pd
-los_angeles_restaurant_health_inspections.loc
+los_angeles_restaurant_health_inspections.loc[los_angeles_restaurant_health_inspections['program_status']=='INACTIVE']
+
